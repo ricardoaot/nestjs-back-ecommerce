ef89fe85cfef79a35b8bad5c9c66d3be85f226ee
ricardoaot <ricardoaot@gmail.com>
2024-06-24 22:19:19 -0500

    L6 - Global Pipe implementation from Class-Validator

--- ecommerce-ricardoaot/commands.md
+++ ecommerce-ricardoaot/commands.md
@@ -1,5 +1,6 @@
 ``` bash
 npm run build           # Compilar codigo en carpeta dist
+npm run migration:create src/migrations/created #Se crea el sql de migración inicial sin datos
 npm run migration:generate src/migrations/initial #Se crea el sql de migración inicial
 npm run build           # Compilar y pasar el archivo de migración a dist
 npm run migration:run   # Ejecución del archivo de migración sql
--- ecommerce-ricardoaot/src/main.ts
+++ ecommerce-ricardoaot/src/main.ts
@@ -3,12 +3,31 @@ import { AppModule } from './app.module';
 import { GlobalLoggerMiddleware } from './middlewares/logger.middleware';
 import { CategoriesController } from './module/categories/categories.controller';
 import { ProductsController } from './module/products/products.controller';
-import { Res } from '@nestjs/common';
+import { BadRequestException, Res, ValidationPipe } from '@nestjs/common';
 import { Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(GlobalLoggerMiddleware)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      //transform: true,
+      exceptionFactory: (errors) => {
+        const cleanedErrors = errors.map((e) => {
+          return {
+            property: e.property,
+            //value: e.value,
+            constraints: e.constraints,
+          };
+        })
+        return new BadRequestException({
+          alert: 'the following errors occurred',
+          errors: cleanedErrors
+        })
+      }
+    })
+  );
 
   // Seeders call
   const categorySeeder = app.get(CategoriesController);
